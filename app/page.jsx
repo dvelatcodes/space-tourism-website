dd946e9b214146053f5336ba430022a2370f88c9
Love Olubummo <olubummolove3@gmail.com>
2023-11-01 16:12:53 +0100

    update: finished desktop view for home-page

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,21 @@ export default function Home() {
   return (
     <div className='homeContainer'>
       <Navbar />
-      <main></main>
+      <main>
+        <div className='homeContent'>
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
+          <p>
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className='explore'>
+          <h4>EXPLORE</h4>
+        </div>
+      </main>
     </div>
   );
 }
